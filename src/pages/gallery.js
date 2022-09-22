e425fed4c8a0c8b2ca662b47d011fecdbbb0e137
“davidemoscatelli” <davidemoscatelli1995@gmail.com>
2022-09-23 00:11:11 +0200

    aggiornamento layout

---
+++ src/pages/gallery.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Frame from "../components/frame"
+
+const GalleryPage = () => {
+    return(
+        <Frame>
+            <p>questo si trova dentro il children</p>
+        </Frame>
+    )
+}
+
+export default GalleryPage
